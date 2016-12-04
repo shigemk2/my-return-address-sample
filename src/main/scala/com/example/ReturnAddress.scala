9a3b6b6d0d0744fb5bee7d97eadd276922f7a453
shigemk2 <i.am.shige@gmail.com>
2016-12-05 02:28:40 +0900

    Add classes

--- src/main/scala/com/example/ReturnAddress.scala
+++ src/main/scala/com/example/ReturnAddress.scala
@@ -4,3 +4,12 @@ import akka.actor._
 
 object ReturnAddressDriver extends CompletableApp(2) {
 }
+
+class Client extends Actor {
+}
+
+class Server extends Actor {
+}
+
+class Worker extends Actor {
+}
