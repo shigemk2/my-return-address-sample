a7fb4b896c51311b55387aeeddbfbd11430266a0
shigemk2 <i.am.shige@gmail.com>
2016-12-05 17:35:14 +0900

    Add receive

--- src/main/scala/com/example/ReturnAddress.scala
+++ src/main/scala/com/example/ReturnAddress.scala
@@ -6,10 +6,16 @@ object ReturnAddressDriver extends CompletableApp(2) {
 }
 
 class Client extends Actor {
+  def receive = {
+  }
 }
 
 class Server extends Actor {
+  def receive = {
+  }
 }
 
 class Worker extends Actor {
+  def receive = {
+  }
 }
