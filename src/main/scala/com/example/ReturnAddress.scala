3a128b6d3e8db9201d9114ad8cc2584abd3ab6a0
shigemk2 <i.am.shige@gmail.com>
2016-12-05 17:38:58 +0900

    Add Client

--- src/main/scala/com/example/ReturnAddress.scala
+++ src/main/scala/com/example/ReturnAddress.scala
@@ -13,6 +13,18 @@ object ReturnAddressDriver extends CompletableApp(2) {
 
 class Client extends Actor {
   def receive = {
+    case StartWith(server) =>
+      println("Client: is starting...")
+      server ! Request("REQ-1")
+      server ! RequestComplex("REQ-20")
+    case Reply(what) =>
+      println("Client: received reply: " + what)
+      ReturnAddressDriver.completedStep()
+    case ReplyToComplex(what) =>
+      println("Client: received reply to complex: " + what)
+      ReturnAddressDriver.completedStep()
+    case _ =>
+      println("Client: received unexpected message")
   }
 }
 
